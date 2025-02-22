3e23e260e41fba11436296f2194c47d6d88873c1
Inflyser <89538138944s@gmail.com>
2025-02-22 09:51:11 +0000

    t9.3

---
+++ static/scripts/films_button.js
@@ -0,0 +1,34 @@
+// Страница №2
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+}
+
+var infoType = getUrlParameter('info');
+
+if (infoType === 'film1') { // Интерстеллар
+    document.getElementById('content').textContent = 'Это первая часть информации.';
+    document.getElementById('name__film').textContent = 'Интерстеллар';
+    document.getElementById('short__text').textContent = '...';
+    document.getElementById('year').textContent = '...';
+    document.getElementById('country').textContent = '...';
+    document.getElementById('genre').textContent = '...';
+
+} else if (infoType === 'film2') {
+    document.getElementById('content').textContent = 'Это вторая часть информации.';
+    document.getElementById('name__film').textContent = 'Унесённые призраками';
+    document.getElementById('short__text').textContent = '...';
+    document.getElementById('year').textContent = '...';
+    document.getElementById('country').textContent = '...';
+    document.getElementById('genre').textContent = '...';
+}
